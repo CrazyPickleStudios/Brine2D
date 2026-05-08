6e00d4de58e75e0184c4f3dc796563f25d69b9f5
RichardPreziosi <17318983+RichardPreziosi@users.noreply.github.com>
2026-05-08 15:30:36 -0400

    checking the next line.

--- tests/Brine2D.Tests/Systems/Physics/PhysicsEngineAdvancedTests.cs
+++ tests/Brine2D.Tests/Systems/Physics/PhysicsEngineAdvancedTests.cs
@@ -62,14 +62,14 @@ public class PhysicsEngineAdvancedTests : PhysicsTestBase
 
         Step(world, system);
         PhysicsWorld.SetContactHitEventThreshold(0f);
-        //dynEntity.GetComponent<PhysicsBodyComponent>()!.LinearVelocity = new Vector2(0f, 5000f);
+        dynEntity.GetComponent<PhysicsBodyComponent>()!.LinearVelocity = new Vector2(0f, 5000f);
 
-        //bool hitFired = false;
-        //dynEntity.GetComponent<PhysicsBodyComponent>()!.OnCollisionHit += (_, contact) =>
-        //{
-        //    if (contact.ImpactSpeed > 0f)
-        //        hitFired = true;
-        //};
+        bool hitFired = false;
+        dynEntity.GetComponent<PhysicsBodyComponent>()!.OnCollisionHit += (_, contact) =>
+        {
+            if (contact.ImpactSpeed > 0f)
+                hitFired = true;
+        };
 
         //Step(world, system, 10);
 
